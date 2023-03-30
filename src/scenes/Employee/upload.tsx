347b1d0f87b231f10f3f5f0989ad918a343fd616
guillermo gabion <guillermo.gabion@actimai.com>
2023-03-30 09:18:36 +0800

    modal fixes

--- src/scenes/Employee/upload.tsx
+++ src/scenes/Employee/upload.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import { async } from 'validate.js';
 import { RequestAPI, Api } from "../../api"
 import http from "../../helpers/axios"
 import { Utility } from "../../utils"
 
-const FileUpload: React.FC = () => {
+type FileUploadProps = {
+  onCloseModal: () => void;
+};
+
+const FileUpload: React.FC<FileUploadProps> = ({ onCloseModal }) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,8 +16,12 @@ const FileUpload: React.FC = () => {
       setSelectedFile(event.target.files[0]);
     }
   };
+  const close = async () => {
+    onCloseModal(); 
+  }
 
   const upload = async () => {
+     
     try {
       if (selectedFile) {
         const formData = new FormData();
@@ -28,6 +37,7 @@ const FileUpload: React.FC = () => {
 
         if (response.status === 200) {
           console.log("File uploaded successfully!");
+          onCloseModal(); 
         } else {
           console.error(`File upload failed with status ${response.status}`);
         }
@@ -38,9 +48,10 @@ const FileUpload: React.FC = () => {
   };
 
   return (
-    <div>
-      <input type="file" accept=".xlsx" onChange={handleFileChange} />
-      <button onClick={upload}>Upload</button>
+    <div  className="form-group">
+      <input type="file" accept=".xlsx" className="file-input-style" onChange={handleFileChange} />
+      <button className="btn btn-primary" onClick={upload}>Upload</button>
+      <button className="btn btn-primary" onClick={close}>Upload</button>
     </div>
   );
 };
