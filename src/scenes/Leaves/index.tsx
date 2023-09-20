cb4e2177654f998b072add20f1449a69bce162d0
April Jay <jay.actimai@actimai.com>
2023-09-20 16:01:57 +0800

    #comment fix no action called in Approving Leave

--- src/scenes/Leaves/index.tsx
+++ src/scenes/Leaves/index.tsx
@@ -439,60 +439,59 @@ export const Leaves = (props: any) => {
       },
     }).then((result) => {
       if (result.isConfirmed) {
-        // Swal.fire({
-        //   title: '',
-        //   allowOutsideClick: false,
-        //   didOpen: () => {
-        //     Swal.showLoading();
-        //   }
-        // });
-        getAllLeaves(0, key)
-        // RequestAPI.postRequest(Api.approveLeave, "", { "id": id }, {}, async (res: any) => {
-        //   const { status, body = { data: {}, error: {} } }: any = res
-        //   if (status === 200 || status === 201) {
-        //     if (body.error && body.error.message) {
-        //       Swal.close()
-        //       ErrorSwal.fire({
-        //         title: 'Error!',
-        //         text: (body.error && body.error.message) || "",
-        //         didOpen: () => {
-        //           const confirmButton = Swal.getConfirmButton();
+        Swal.fire({
+          title: '',
+          allowOutsideClick: false,
+          didOpen: () => {
+            Swal.showLoading();
+          }
+        });
+        RequestAPI.postRequest(Api.approveLeave, "", { "id": id }, {}, async (res: any) => {
+          const { status, body = { data: {}, error: {} } }: any = res
+          if (status === 200 || status === 201) {
+            if (body.error && body.error.message) {
+              Swal.close()
+              ErrorSwal.fire({
+                title: 'Error!',
+                text: (body.error && body.error.message) || "",
+                didOpen: () => {
+                  const confirmButton = Swal.getConfirmButton();
 
-        //           if (confirmButton)
-        //             confirmButton.id = "login_errorconfirm2_alertbtn"
-        //         },
-        //         icon: 'error',
-        //       })
-        //     } else {
-        //       Swal.close()
-        //       ErrorSwal.fire({
-        //         title: 'Success!',
-        //         text: (body.data) || "",
-        //         didOpen: () => {
-        //           const confirmButton = Swal.getConfirmButton();
+                  if (confirmButton)
+                    confirmButton.id = "login_errorconfirm2_alertbtn"
+                },
+                icon: 'error',
+              })
+            } else {
+              Swal.close()
+              ErrorSwal.fire({
+                title: 'Success!',
+                text: (body.data) || "",
+                didOpen: () => {
+                  const confirmButton = Swal.getConfirmButton();
 
-        //           if (confirmButton)
-        //             confirmButton.id = "login_successconfirm_alertbtn"
-        //         },
-        //         icon: 'success',
-        //       })
-        //       getAllLeaves(0, key)
-        //     }
-        //   } else {
-        //     Swal.close()
-        //     ErrorSwal.fire({
-        //       title: 'Error!',
-        //       text: "Something Error.",
-        //       didOpen: () => {
-        //         const confirmButton = Swal.getConfirmButton();
+                  if (confirmButton)
+                    confirmButton.id = "login_successconfirm_alertbtn"
+                },
+                icon: 'success',
+              })
+              getAllLeaves(0, key)
+            }
+          } else {
+            Swal.close()
+            ErrorSwal.fire({
+              title: 'Error!',
+              text: "Something Error.",
+              didOpen: () => {
+                const confirmButton = Swal.getConfirmButton();
 
-        //         if (confirmButton)
-        //           confirmButton.id = "login_errorconfirm3_alertbtn"
-        //       },
-        //       icon: 'error',
-        //     })
-        //   }
-        // })
+                if (confirmButton)
+                  confirmButton.id = "login_errorconfirm3_alertbtn"
+              },
+              icon: 'error',
+            })
+          }
+        })
       }
     })
   }
